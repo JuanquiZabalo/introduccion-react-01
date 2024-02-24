028753c668df4f62a0f2e6d53561021d44eefa42
JuanquiZabalo <jucazato95@gmail.com>
2024-02-24 19:16:19 +0100

    Agregar contador a ejercicio 4

---
+++ src/components/Ejercicio4/Ejercicio4.jsx
@@ -0,0 +1,11 @@
+import TituloEjercicio from "../TituloEjercicio";
+import Contador from "./Contador";
+
+export default function Ejercicio4(){
+    return(
+        <section className="caja-ejercicio">
+            <TituloEjercicio>Este es el Ejercicio 4</TituloEjercicio>
+            <Contador/>
+        </section>
+    )
+}
