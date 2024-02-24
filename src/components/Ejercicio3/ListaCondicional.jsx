d8a189c19a5dab7cff1a9b4541d0efec3a62a3c4
JuanquiZabalo <jucazato95@gmail.com>
2024-02-24 18:10:05 +0100

    Crear componente lista condicional

---
+++ src/components/Ejercicio3/ListaCondicional.jsx
@@ -0,0 +1,16 @@
+export default function ListaProductosCondicional(prop){
+    return(
+        <div>
+            <h3>Lista de productos condicional</h3>
+            {prop.productos.length ===0 ? (
+                <p>No hay elementos en la lista</p>
+            ) : (
+                <ul>
+                    {prop.productos.map((producto, index)=>{
+                        return <li key={index}> {producto}</li>
+                    })}
+                </ul>
+            )}
+        </div>
+    )
+}
