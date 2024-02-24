9ceb19a6810d4715e9fe0734794bce3e7de6915f
JuanquiZabalo <jucazato95@gmail.com>
2024-02-24 19:16:27 +0100

    actualizar app

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import TituloPrincipal from './components/TituloPrincipal'
 import './index.css'
 import Ejercicio2 from './components/Ejercicio2/Ejercicio2'
 import Ejercicio3 from './components/Ejercicio3/Ejercicio3'
+import Ejercicio4 from './components/Ejercicio4/Ejercicio4'
+
 
 export default function App() {
   return (
@@ -16,6 +18,7 @@ export default function App() {
         <Ejercicio1 />
         <Ejercicio2/>
         <Ejercicio3/>
+        <Ejercicio4/>
       </main>
       <Footer />
     </>
