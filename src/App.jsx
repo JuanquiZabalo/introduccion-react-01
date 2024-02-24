a67e311d63fc3ee4f37ad7798d8207c7f67b70a7
JuanquiZabalo <jucazato95@gmail.com>
2024-02-24 18:10:55 +0100

    Actualizar app

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import Footer from './components/Footer'
 import Header from './components/Header'
 import TituloPrincipal from './components/TituloPrincipal'
 import './index.css'
-
+import Ejercicio2 from './components/Ejercicio2/Ejercicio2'
+import Ejercicio3 from './components/Ejercicio3/Ejercicio3'
 export default function App() {
   return (
     <>
       <Header />
       <main>
         <TituloPrincipal />
-        <Ejercicio1 />
         {/* Añade tus componentes aqui */}
+        <Ejercicio1 />
+        <Ejercicio2/>
+        <Ejercicio3/>
       </main>
       <Footer />
     </>
