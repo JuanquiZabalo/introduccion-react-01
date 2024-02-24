a870042d2a80f7fa48bcdde99d8ee2a89954ed41
JuanquiZabalo <jucazato95@gmail.com>
2024-02-24 18:42:57 +0100

    actualizar app

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import TituloPrincipal from './components/TituloPrincipal'
 import './index.css'
 import Ejercicio2 from './components/Ejercicio2/Ejercicio2'
 import Ejercicio3 from './components/Ejercicio3/Ejercicio3'
+
 export default function App() {
   return (
     <>
