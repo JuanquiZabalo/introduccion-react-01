e8dd30f12f94a0b3990f8a2a5c68fec1aac994bd
JuanquiZabalo <jucazato95@gmail.com>
2024-02-24 19:16:06 +0100

    crear componente contador

---
+++ src/components/Ejercicio4/Contador.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export default function Contador(){
+    const [contador, setContador] = useState(0)
+
+    return(
+        <button onClick={setContador(contador+1)}>
+            Contador: {contador} 
+        </button>
+    )
+
+}
