17d478119855a7c77592b0a0fbd6a92f961330a2
JuanquiZabalo <jucazato95@gmail.com>
2024-02-24 18:42:28 +0100

    actualizar componente

--- src/components/Ejercicio3/ListaCondicional.jsx
+++ src/components/Ejercicio3/ListaCondicional.jsx
@@ -2,12 +2,12 @@ export default function ListaProductosCondicional(prop){
     return(
         <div>
             <h3>Lista de productos condicional</h3>
-            {prop.productos.length ===0 ? (
+            {prop.productos.length === 0 ? (
                 <p>No hay elementos en la lista</p>
             ) : (
                 <ul>
                     {prop.productos.map((producto, index)=>{
-                        return <li key={index}> {producto}</li>
+                        return <li key={index}> {producto} </li>
                     })}
                 </ul>
             )}
